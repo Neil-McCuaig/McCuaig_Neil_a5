8f424c1284986b9e02a614cadb6d50a770584f1c
Neil-McCuaig <Neil_McCuaig@bell.net>
2023-12-06 18:53:19 -0500

    Added player controls
    Added a check to the player script for if the wasd keys or the arrow keys are being pressed, that should make it turn.

--- Asteroids/Assets/Scripts/Player.cs
+++ Asteroids/Assets/Scripts/Player.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public float thrustSpeed = 1.0f;
+    public float turnSpeed = 1.0f;
+
+    // The _ in front are because I'm not sure if it will conflict with anything if I just call it "rigidbody" for example. Better safe then sorry.
+
+    private Rigidbody2D _rigidbody;
+    private bool _thrust;
+    private float _turnDirection;
+
+    private void Awake()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Input checks
+        _thrust = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow);
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            _turnDirection = 1.0f;
+        } else if (Input.GetKey(KeyCode.D)  || Input.GetKey(KeyCode.RightArrow)) 
+        { 
+            _turnDirection = -1.0f; 
+        } else 
+        { 
+            _turnDirection = 0.0f; 
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // .up makes it go forward, not "Up" as in the top of the screen.
+        if (_thrust)
+        {
+            _rigidbody.AddForce(this.transform.up * this.thrustSpeed);
+        }
+        if (_turnDirection != 0.0)
+        {
+            _rigidbody.AddTorque(_turnDirection * this.turnSpeed);
+        }
     }
 }
